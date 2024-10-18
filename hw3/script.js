f463e884441cda8510ce02c472b77ced992530c6
w3ird0412 <110398@stu.hgsh.hc.edu.tw>
2024-10-18 11:46:52 +0800

    Update script.js

--- hw3/script.js
+++ hw3/script.js
@@ -1,7 +1,7 @@
 // 定義變數
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-let x = [17, canvas.width, canvas.width, 28], y = [0, 11, canvas.height, canvas.height], dx = [5, -5, -5, 5], dy = [5, 5, -5, -5], r = [30, 30, 30, 30], color = ["#CE0000", "#000000", "#FF8000", "#FF95CA"];
+let x = [117, canvas.width, canvas.width, 128], y = [0, 11, canvas.height, canvas.height], dx = [5, -5, -5, 5], dy = [5, 5, -5, -5], r = [30, 30, 30, 30], color = ["#CE0000", "#000000", "#FF8000", "#FF95CA"];
 let n = 4;
 
 
